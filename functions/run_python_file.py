b3651f7dcb7d944565c055d252c226fa032d4e82
Cody Milliron <codymilliron.dev@gmail.com>
2025-11-13 23:35:25 -0500

    add ability to run python code

---
+++ functions/run_python_file.py
@@ -0,0 +1,26 @@
+import os, subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    full_path = os.path.join(working_directory, file_path)
+    if not os.path.abspath(full_path).startswith((os.path.abspath(working_directory))):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(full_path):
+        return f'Error: File "{file_path}" not found.'
+    if os.path.basename(full_path).split(".")[1] != "py":
+        return f'Error: "{file_path}" is not a Python file.'
+    # print(subprocess.run("pwd", capture_output=True, cwd=os.path.abspath(working_directory)))
+    try:
+        result = subprocess.run(
+            ["uv", "run", full_path, *args], 
+            capture_output=True, 
+            timeout=30, 
+            # cwd=os.path.abspath(working_directory)
+            )
+        output_string = f"Output:\nSTDOUT: {result.stdout}\nSTDERR: {result.stderr}\n"
+        if result.returncode != 0:
+            output_string = output_string + f"Process exited with code {result.returncode}"
+        # if len(result) == 0:
+        #     return "No Output produced"
+        return output_string
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 
 def test_get_files_info():
@@ -32,8 +33,22 @@ def test_write_file():
     print("Test 3:")
     print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
+def run_python_tests():
+    print("Test 1")
+    print(run_python_file("calculator", "main.py"))
+    print("Test 2")
+    print(run_python_file("calculator", "main.py", ["3 + 5"]))
+    print("Test 3")
+    print(run_python_file("calculator", "tests.py"))
+    print("Test 4")
+    print(run_python_file("calculator", "../main.py"))
+    print("Test 5")
+    print(run_python_file("calculator", "nonexistent.py"))
+    print("Test 6")
+    print(run_python_file("calculator", "lorem.txt"))
+
 def main ():
-    test_write_file()
+    run_python_tests()
 
 
 if __name__ == "__main__":
