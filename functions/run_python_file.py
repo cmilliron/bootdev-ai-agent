8f9dc61bd10a3fc1268d68fc069062f95003ef11
Cody Milliron <codymilliron.dev@gmail.com>
2025-11-17 21:31:32 -0500

    Add some requirements and update run_file schema

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -3,19 +3,24 @@ from google.genai import types
 
 schema_run_python_file = types.FunctionDeclaration(
     name="run_python_file",
-    description="Executes a python program at file_path with optional args, constrained to the working directory.",
+    description="Executes a Python file within the working directory and returns the output from the interpreter.",
     parameters=types.Schema(
         type=types.Type.OBJECT,
         properties={
             "file_path": types.Schema(
                 type=types.Type.STRING,
-                description="The path to the file to execute. Required. Constrained by the working directory."
+                description="The path to the file to execute. Required. Relative to the working directory."
             ),
-            # "args": types.Schema(
-            #     type=types.Type.ARRAY,
-            #     description="These are optional arguments for the program.",
-            # ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(
+                    type=types.Type.STRING,
+                    description="Optional arguments to pass to the Python file.",
+                ),
+                description="Optional arguments to pass to the Python file.",
+            ),
         },
+        required=["file_path"],
     ),
 )
 
