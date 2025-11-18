03e156aded0a3f64da9f669e22f5c1f5bd6f9b18
Cody Milliron <codymilliron.dev@gmail.com>
2025-11-17 20:42:12 -0500

    add ability to call get_file_content for agent

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,21 @@
 import os
+from google.genai import types
 # from config import MAX_CHARS
+MAX_CHARS = 10000
+
+sechema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Get contents of a specified file,  constrained to the working directory",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to read content from. must be provided."
+            )
+        }
+    )
+)
 
 def get_file_content(working_directory, file_path):
     full_path = os.path.join(working_directory, file_path)
@@ -8,9 +24,9 @@ def get_file_content(working_directory, file_path):
     if not os.path.isfile(full_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
     with open(full_path, "r") as f:
-        content = f.read(10000)
-    if len(content) == 10000:
-        return f"{content[:10000]}...file \"{os.path.basename(file_path)}\""
+        content = f.read(MAX_CHARS)
+    if len(content) == MAX_CHARS:
+        return f"{content[:MAX_CHARS]}...file \"{os.path.basename(file_path)}\""
     return content
 
 def main():
