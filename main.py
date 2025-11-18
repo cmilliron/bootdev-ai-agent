706e38e48c6fdf7bad7b7ad78784b4a12b60f78d
Cody Milliron <codymilliron.dev@gmail.com>
2025-11-17 22:33:50 -0500

    add ability to call functions

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from google import genai
 from google.genai import types
 import sys
 from functions.get_files_info import get_files_info, schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.write_file import schema_write_file
-from functions.run_python_file import schema_run_python_file
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.write_file import write_file, schema_write_file
+from functions.run_python_file import run_python_file, schema_run_python_file
 from prompts import system_prompt
 import pprint
 
@@ -19,12 +19,44 @@ available_functions = types.Tool(
         schema_get_files_info,
         schema_get_file_content,
         schema_write_file,
-        schema_run_python_file
+        schema_run_python_file,
     ]
 )
 
+function_dictionary = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "write_file": write_file,
+    "run_python_file": run_python_file,
+}
 
-
+def call_functions(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+    if function_call_part.name not in function_dictionary:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"error": f"Unknown function: {function_call_part.name}"},
+                )
+            ],
+        )
+    else:
+        function_response = function_dictionary[function_call_part.name](working_directory="./calculator", **function_call_part.args)
+        print(function_response)
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"result": function_response},
+                )
+            ],
+        )
 
 
 def check_for_verbose_flag(args):
@@ -35,9 +67,17 @@ def check_for_verbose_flag(args):
 
 def format_output(prompt, response, verbose_flag):
     print(f"User prompt: {prompt}") if verbose_flag else None
-    print("Verbose: ",response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}") if verbose_flag else None
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}") if verbose_flag else None
+    print("Verbose: ", response.text)
+    (
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        if verbose_flag
+        else None
+    )
+    (
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if verbose_flag
+        else None
+    )
 
 
 def main():
@@ -46,33 +86,32 @@ def main():
         verbose_flag = check_for_verbose_flag(sys.argv)
         print(f"Hello from bootdev-ai-agent!\nPrompt: {user_prompt}")
         # print(verbose_flag)
-        messages = [
-            types.Content(
-                role="user",
-                parts=[types.Part(text=user_prompt)]
-            )
-        ]
+        messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
         # prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
         response = client.models.generate_content(
-            model='gemini-2.0-flash-001', 
+            model="gemini-2.0-flash-001",
             contents=messages,
-            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
-        # pprint.pprint(response.function_calls)         
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+            ),
+        )
+        # pprint.pprint(response.function_calls)
         print(f"User prompt: {user_prompt}") if verbose_flag else None
         if response.function_calls != None:
-            print("got functioni calls")
-            # print(response.function_calls)
+             # print(response.function_calls)
             for item in response.function_calls:
-                # print(item)
-                print(f"Calling function: {item.name}({item.args})")
+                 # print(f"Calling function: {item.name}({item.args})")
+                call_functions(item,  verbose=True)
         else:
             print("Response:", response.text)
         if verbose_flag:
             print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        
+
     except IndexError as index_error:
-        print("No prompt provided.\nRun again but provide prompt. Example: uv run main.py <prompt>")
+        print(
+            "No prompt provided.\nRun again but provide prompt. Example: uv run main.py <prompt>"
+        )
         sys.exit(1)
 
 
